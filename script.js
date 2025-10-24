8c8eefd930424868f02d4c040f9e3c17fa8bdcfe
Kani <zulumovakanykei54@gmail.com>
2025-10-25 03:42:31 +0600

    #bacis logic

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 let board = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let isGameContinuing = true;
+let playerX = {
+  name: "",
+  symbol: "X",
+};
+let playerO = {
+  name: "",
+  symbol: "O",
+};
+
+function getCurrentPlayerObject() {
+  if (currentPlayer === "X") {
+    return playerX;
+  } else {
+    return playerO;
+  }
+}
 const boardDiv = document.querySelector(".board");
 const playerName = document.querySelector(".playerName");
 const warningDiv = document.querySelector(".warning");
 const gameResult = document.querySelector(".gameResult");
-playerName.textContent = `${currentPlayer}'s turn`;
+const resetButton = document.querySelector(".reset");
+const startButton = document.querySelector(".start");
+const divReset = document.querySelector(".resetGame");
 
 function renderBoard() {
   for (let i = 0; i < board.length; i++) {
@@ -36,12 +54,16 @@ boardDiv.addEventListener("click", (e) => {
     for (const combination of winningCombinations) {
       const [a, b, c] = combination;
       if (board[a] == board[b] && board[b] == board[c] && board[a] !== "") {
-        gameResult.textContent = `Conglatulations ${currentPlayer}, you win!`;
+        playerName.textContent = "";
+        gameResult.textContent = `Conglatulations ${
+          getCurrentPlayerObject().name
+        }, you win!`;
         isGameContinuing = false;
       }
     }
     if (board.every((cell) => cell !== "")) {
       gameResult.textContent = `It's a tie`;
+      playerName.textContent = "";
       isGameContinuing = false;
     }
 
@@ -59,29 +81,62 @@ boardDiv.addEventListener("click", (e) => {
     warningDiv.textContent = "Choose another cell";
   }
   if (isGameContinuing) {
-    playerName.textContent = `${currentPlayer}'s turn`;
+    playerName.textContent = `${getCurrentPlayerObject().name}'s turn`;
   }
 
   renderBoard();
 });
 
-//
+function resetGame() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = "X";
+  isGameContinuing = true;
+  gameResult.textContent = "";
+  warningDiv.textContent = "";
+  playerName.textContent = `${playerX.name}'s turn`;
+  renderBoard();
+}
+resetButton.addEventListener("click", resetGame);
+resetGame();
 
-//
+startButton.addEventListener("click", function () {
+  favDialog.showModal();
+});
+const cancelButton = document.getElementById("cancel");
+cancelButton.addEventListener("click", function () {
+  favDialog.close();
+});
 
-//
-
-//
-
-/*
-
-
-
-
-*/
-console.log(`
-    ${board[0]} | ${board[1]} | ${board[2]}
-    ---------
-    ${board[3]} | ${board[4]} | ${board[5]}
-    ---------
-    ${board[6]} | ${board[7]} | ${board[8]}`);
+const form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const playerInfo = document.getElementById("nameone");
+  const playerInfo2 = document.getElementById("nametow");
+  const symbolX = document.getElementById("symbolone");
+  const symbolO = document.getElementById("symboltwo");
+  const namePlayer1 = playerInfo.value;
+  const namePlayer2 = playerInfo2.value;
+  const symbolPlayer1 = symbolX.value;
+  const symbolPlayer2 = symbolO.value;
+  if (
+    symbolPlayer1 === "" ||
+    symbolPlayer2 === "" ||
+    symbolPlayer1 === symbolPlayer2
+  ) {
+    warningDiv.textContent = "Choose different symbols!";
+    return;
+  }
+  if (symbolPlayer1 === symbolX.value) {
+    playerX.name = namePlayer1;
+    playerO.name = namePlayer2;
+  } else {
+    playerX.name = namePlayer2;
+    playerO.name = namePlayer1;
+  }
+  playerX.name = namePlayer1;
+  playerO.name = namePlayer2;
+  playerX.symbol = symbolPlayer1;
+  playerO.symbol = symbolPlayer2;
+  favDialog.close();
+  resetGame();
+});
