295dbfa8303cbf463f41deb2fc5e6f4e2b959143
Kani <zulumovakanykei54@gmail.com>
2025-10-25 03:43:05 +0600

    #factory function & modules refactoring

--- script.js
+++ script.js
@@ -1,45 +1,53 @@
-let board = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X";
-let isGameContinuing = true;
-let playerX = {
-  name: "",
-  symbol: "X",
-};
-let playerO = {
-  name: "",
-  symbol: "O",
-};
+const Gameboard = (function () {
+  let board = ["", "", "", "", "", "", "", "", ""];
+  const placeMark = (index, symbol) => {
+    if (board[index] === "") {
+      board[index] = symbol;
+      return true;
+    }
+    return false;
+  };
+  const resetBoard = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+  };
+  const getBoard = () => {
+    return board;
+  };
 
-function getCurrentPlayerObject() {
-  if (currentPlayer === "X") {
-    return playerX;
-  } else {
-    return playerO;
-  }
-}
-const boardDiv = document.querySelector(".board");
-const playerName = document.querySelector(".playerName");
-const warningDiv = document.querySelector(".warning");
-const gameResult = document.querySelector(".gameResult");
-const resetButton = document.querySelector(".reset");
-const startButton = document.querySelector(".start");
-const divReset = document.querySelector(".resetGame");
+  return {
+    placeMark,
+    resetBoard,
+    getBoard,
+  };
+})();
 
-function renderBoard() {
-  for (let i = 0; i < board.length; i++) {
-    const playerMove = board[i];
-    const selector = `[data-index="${i}"]`;
-    const cellElement = boardDiv.querySelector(selector);
-    cellElement.textContent = playerMove;
-  }
-}
-renderBoard();
+const GameController = (function () {
+  let currentPlayer = "X";
+  let isGameContinuing = true;
+  let _playerOne = null;
+  let _playerTwo = null;
+  const resetGame = () => {
+    currentPlayer = "X";
+    isGameContinuing = true;
+  };
 
-boardDiv.addEventListener("click", (e) => {
-  const clickedCell = e.target;
-  let index = e.target.dataset.index;
-  if (board[index] === "" && isGameContinuing) {
-    board[index] = currentPlayer;
+  const setPlayers = (p1, p2) => {
+    _playerOne = p1;
+    _playerTwo = p2;
+  };
+
+  const _getPlayerBySymbol = (symbol) => {
+    if (_playerOne.symbol === symbol) {
+      return _playerOne;
+    } else {
+      return _playerTwo;
+    }
+  };
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  };
+  const checkWin = (board) => {
     const winningCombinations = [
       [0, 1, 2],
       [3, 4, 5],
@@ -53,90 +61,122 @@ boardDiv.addEventListener("click", (e) => {
 
     for (const combination of winningCombinations) {
       const [a, b, c] = combination;
-      if (board[a] == board[b] && board[b] == board[c] && board[a] !== "") {
-        playerName.textContent = "";
-        gameResult.textContent = `Conglatulations ${
-          getCurrentPlayerObject().name
-        }, you win!`;
-        isGameContinuing = false;
+      if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+        return true;
       }
     }
-    if (board.every((cell) => cell !== "")) {
-      gameResult.textContent = `It's a tie`;
+    return false;
+  };
+  const checkTie = (board) => {
+    return board.every((cell) => cell !== "");
+  };
+
+  const playRound = (index) => {
+    const didTheMoveWork = Gameboard.placeMark(index, currentPlayer);
+    if (didTheMoveWork) {
+      const currentBoard = Gameboard.getBoard();
+      if (checkWin(currentBoard)) {
+        return "win";
+      }
+      if (checkTie(currentBoard)) {
+        return "tie";
+      }
+      switchPlayer();
+      return "continue";
+    } else {
+      return "invalid";
+    }
+  };
+  return {
+    playRound: playRound,
+    getCurrentPlayer: () => _getPlayerBySymbol(currentPlayer),
+    resetGame: resetGame,
+    setPlayers: setPlayers,
+  };
+})();
+
+const displayController = (function () {
+  const boardDiv = document.querySelector(".board");
+  const gameResult = document.querySelector(".gameResult");
+  const warningDiv = document.querySelector(".warning");
+  const playerName = document.querySelector(".playerName");
+  boardDiv.addEventListener("click", (e) => {
+    const clickedCell = e.target;
+    let index = e.target.dataset.index;
+    const roundStatus = GameController.playRound(index);
+    if (roundStatus === "win") {
+      renderBoard();
+      let playerInfo = GameController.getCurrentPlayer();
+      gameResult.textContent = `Conglatulations ${playerInfo.name}, you win!`;
       playerName.textContent = "";
-      isGameContinuing = false;
+    } else if (roundStatus === "tie") {
+      renderBoard();
+      gameResult.textContent = `It's a tie`;
+    } else if (roundStatus === "continue") {
+      renderBoard();
+      let playerInfo = GameController.getCurrentPlayer();
+      playerName.textContent = `${playerName.name}'s turn`;
+    } else if (roundStatus === "invalid") {
+      warningDiv.textContent = "Choose another cell";
     }
-
-    if (isGameContinuing === true) {
-      switch (currentPlayer) {
-        case "X":
-          currentPlayer = "O";
-          break;
-        case "O":
-          currentPlayer = "X";
-          break;
-      }
+  });
+  const renderBoard = () => {
+    const board = Gameboard.getBoard();
+    for (let i = 0; i < board.length; i++) {
+      const selector = `[data-index="${i}"]`;
+      const cellElement = boardDiv.querySelector(selector);
+      cellElement.textContent = board[i];
     }
-  } else {
-    warningDiv.textContent = "Choose another cell";
-  }
-  if (isGameContinuing) {
-    playerName.textContent = `${getCurrentPlayerObject().name}'s turn`;
-  }
+  };
 
-  renderBoard();
-});
+  const resetButton = document.querySelector(".reset");
+  const startButton = document.querySelector(".start");
 
-function resetGame() {
-  board = ["", "", "", "", "", "", "", "", ""];
-  currentPlayer = "X";
-  isGameContinuing = true;
-  gameResult.textContent = "";
-  warningDiv.textContent = "";
-  playerName.textContent = `${playerX.name}'s turn`;
-  renderBoard();
-}
-resetButton.addEventListener("click", resetGame);
-resetGame();
+  resetButton.addEventListener("click", () => {
+    Gameboard.resetBoard();
+    GameController.resetGame();
+    renderBoard();
+    gameResult.textContent = "";
+    warningDiv.textContent = "";
+  });
 
-startButton.addEventListener("click", function () {
-  favDialog.showModal();
-});
-const cancelButton = document.getElementById("cancel");
-cancelButton.addEventListener("click", function () {
-  favDialog.close();
-});
+  startButton.addEventListener("click", function () {
+    favDialog.showModal();
+  });
+  const cancelButton = document.getElementById("cancel");
+  cancelButton.addEventListener("click", function () {
+    favDialog.close();
+  });
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const playerInfo = document.getElementById("nameone");
+    const playerInfo2 = document.getElementById("nametow");
+    const symbolX = document.getElementById("symbolone");
+    const symbolO = document.getElementById("symboltwo");
+    const namePlayer1 = playerInfo.value;
+    const namePlayer2 = playerInfo2.value;
+    const symbolPlayer1 = symbolX.value;
+    const symbolPlayer2 = symbolO.value;
+    const player1 = createPlayer(namePlayer1, symbolPlayer1);
+    const player2 = createPlayer(namePlayer2, symbolPlayer2);
+    GameController.setPlayers(player1, player2);
+    Gameboard.resetBoard();
+    GameController.resetGame();
+    renderBoard();
+    gameResult.textContent = "";
+    warningDiv.textContent = "";
+    let startingPlayer = GameController.getCurrentPlayer();
+    playerName.textContent = `${startingPlayer.name}'s turn`;
+    favDialog.close();
+  });
 
-const form = document.querySelector("form");
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const playerInfo = document.getElementById("nameone");
-  const playerInfo2 = document.getElementById("nametow");
-  const symbolX = document.getElementById("symbolone");
-  const symbolO = document.getElementById("symboltwo");
-  const namePlayer1 = playerInfo.value;
-  const namePlayer2 = playerInfo2.value;
-  const symbolPlayer1 = symbolX.value;
-  const symbolPlayer2 = symbolO.value;
-  if (
-    symbolPlayer1 === "" ||
-    symbolPlayer2 === "" ||
-    symbolPlayer1 === symbolPlayer2
-  ) {
-    warningDiv.textContent = "Choose different symbols!";
-    return;
-  }
-  if (symbolPlayer1 === symbolX.value) {
-    playerX.name = namePlayer1;
-    playerO.name = namePlayer2;
-  } else {
-    playerX.name = namePlayer2;
-    playerO.name = namePlayer1;
-  }
-  playerX.name = namePlayer1;
-  playerO.name = namePlayer2;
-  playerX.symbol = symbolPlayer1;
-  playerO.symbol = symbolPlayer2;
-  favDialog.close();
-  resetGame();
-});
+  return {};
+})();
+
+const createPlayer = (name, symbol) => {
+  return {
+    name: name,
+    symbol: symbol,
+  };
+};
